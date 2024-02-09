2a00ecb73ce15724f2aac021de6d61a70c73af26
Michał <45739883+michalnaskrecki@users.noreply.github.com>
2024-02-09 18:53:04 +0100

    added totalPrice function

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -5,17 +5,24 @@ import { ConfiguratorContext } from "@/app/store/CommonApi";
 const Resume = () => {
   const ctx = useContext(ConfiguratorContext);
 
-  // const getTotalPrice = () => {
-  //   let totalPrice = 0;
-  //   if (ctx?.selectedModel) totalPrice += ctx?.selectedModel.price;
-  //   if (ctx?.selectedEngine && typeof ctx?.selectedEngine.price)
-  //     totalPrice += ctx?.selectedEngine.price;
-  //   if (ctx?.selectedVersion) totalPrice += ctx?.selectedVersion.price;
-  //   if (ctx?.selectedUpholstery) totalPrice += ctx?.selectedUpholstery.price;
-  //   if (ctx?.selectedAdditionalOption)
-  //     totalPrice += ctx?.selectedAdditionalOption.price;
-  //   return totalPrice;
-  // };
+  const getTotalPrice = () => {
+    let totalPrice = 0;
+    if (ctx?.selectedModel) totalPrice += ctx?.selectedModel.price;
+    if (ctx?.selectedEngine && typeof ctx?.selectedEngine.price === "number")
+      totalPrice += ctx?.selectedEngine.price;
+    if (ctx?.selectedVersion && typeof ctx?.selectedVersion.price === "number")
+      totalPrice += ctx?.selectedVersion.price;
+    if (
+      ctx?.selectedUpholstery &&
+      typeof ctx?.selectedUpholstery.price === "number"
+    )
+      totalPrice += ctx?.selectedUpholstery.price;
+    if (ctx?.selectedAdditionalOption)
+      totalPrice += ctx?.selectedAdditionalOption.price;
+    return totalPrice;
+  };
+
+  console.log(ctx?.selectedEngine);
 
   return (
     <div className="flex m-2 justify-end">
@@ -23,7 +30,7 @@ const Resume = () => {
         wybrany model: <strong>{ctx.selectedModel?.name}</strong>
       </h2>
       <h5 className="m-2">
-        cena: <strong>{ctx.selectedModel?.price}</strong> zł
+        cena: <strong>{getTotalPrice()}</strong> zł
       </h5>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         zamów
